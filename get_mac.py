ab236411a1722c7d83df48acbb48b23060a2754f
i1409 <israel.14@outlook.com>
2020-09-25 16:34:56 -0500

    Usage

--- get_mac.py
+++ get_mac.py
@@ -2,6 +2,8 @@
 from argparse import ArgumentParser
 import sys
 from pexpect import pxssh
+import pty
+
 banner = """
     .------..------..------..------..------..------..------.
     |I.--. ||H.--. ||Y.--. ||P.--. ||E.--. ||R.--. ||X.--. |
@@ -12,8 +14,9 @@ banner = """
 """
 usage_="""
 
-    ~$ get_mac.py -a 192.168.1.1
-    ~$ get_mac.py -s 192.168.1.1 10.1.1.1
+    ~$ python3 get_mac.py -h    |   Show the help
+    ~$ python3 get_mac.py -a 192.168.1.1    |   Get ARP TABLE from 192.168.1.1
+    ~$ python3 get_mac.py -s 192.168.1.1 10.1.1.1   |   Get the ARP REGISTERED of 10.1.1.1 from 192.168.1.1
 """
 cli = pxssh.pxssh()
 parser = ArgumentParser(description='%(prog)s is an ARP Finder\n',usage=usage_)
@@ -43,12 +46,15 @@ def main():
     parser.add_argument('-a', '--all', help='This option gets ALL ARP TABLE from the host given')
     parser.add_argument('-s', '--search', nargs=2, help='This option searches an ARP INFO from the host given based on an IP to look for')
     args = parser.parse_args()
-    cmd = "show arp switch0 | grep "
+    if args.all is None and args.search is None:
+        print(parser.usage)
+        exit()
+    cmd = "show arp  | grep "
     if args.all:
-        cmd += ':'
+        cmd += ': | sort'
         host = args.all
     if args.search:
-        cmd += args.search[1]
+        cmd += args.search[1] + ' | sort'
         host = args.search[0]
     username = input("Enter username-> ")
     passwd = input("Enter password-> ")
